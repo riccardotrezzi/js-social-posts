e8873dc599ce5278a9d4dbb49687e93c8a683ded
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-09-02 08:36:51 +0200

    Esercizio finito con parte di bonus

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Creazione Post
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +57,90 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const likedPost = [];
+
+const postsList = document.getElementById('container');
+
+for(let i = 0; i < posts.length; i++) {
+    
+    //console.log (posts[i]);
+
+    postsList.innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${posts[i].content}</div>
+            <div class="post__image">
+                <img src="${posts[i].media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${posts[i].id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${posts[i].id}" class="js-likes-counter">${posts[i].likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `;
+}
+
+// Bottoni del Post
+
+const likeButton = document.querySelectorAll('.js-like-button');
+console.log('likeButton', likeButton, typeof likeButton.length);
+
+for (let i = 0; i < likeButton.length; i++){
+    likeButton[i].addEventListener('click', function (e) {
+        // console.log('Cliccato Mi Piace');
+
+        e.preventDefault();
+
+        //if(this.classList.contains('like-button--liked') == false) ---- Due opzioni di scrivere la stessa cosa ---- 1. Booleano 2. ! = not - forma Breve
+        if(!this.classList.contains('like-button--liked')){
+            //console.log('this', this, typeof this);
+
+            this.classList.add('like-button--liked');
+
+            //const postId = this.getAttribute('data-postid');
+            const postId = this.dataset.postid;
+
+            //console.log('ID Post n:',  postId);
+            //console.log('ID Counter:', 'like-counter-' + postId);
+
+            const counterElem = document.getElementById('like-counter-' + postId);
+            let likesCount = parseInt(counterElem.innerText);
+            likesCount++;
+            counterElem.innerHTML = likesCount;
+
+            likedPost.push(postId);
+            console.log('Hai messo Mi Piace ai seguenti post: ', likedPost);
+        } 
+        else{ // Bonus
+            this.classList.remove('like-button--liked'); 
+
+            const postId = this.dataset.postid;
+            const counterElem = document.getElementById('like-counter-' + postId);
+            let likesCount = parseInt(counterElem.innerText);
+            likesCount--;
+            counterElem.innerHTML = likesCount;
+        }
+         
+    });
+}
+
